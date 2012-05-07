bb418e67b346e10b1fac5495a2ffe1e209b09d55
Ben Gelb <beg5670@gmail.com>
2012-05-07 11:25:03 -0400

    threads

---
+++ src/Crawler.java
@@ -0,0 +1,40 @@
+import java.io.IOException;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Crawler {
+    private String subr;
+    private String attr;
+    private Document doc;
+    private Elements urls;
+    
+    private Crawler(String subr, String attr) throws IOException {
+        this.subr = subr;
+        this.attr = attr;
+        this.doc = this.connect();              
+        this.set_urls();        
+    }
+    
+    private void set_urls() {
+        urls = this.doc.select(attr);
+    }
+    
+    public Elements get_urls() {
+        return urls;
+    }
+    
+    private Document connect() throws IOException {
+        return Jsoup.connect(subr).get();
+    }
+    
+    public static void main(String args[]) throws IOException {
+        String r1 = "http://www.reddit.com/r/Trees";
+        String ahref = "a[href]";
+        String src = "src";
+        Crawler c = new Crawler(r1, ahref);
+        System.out.println(c.get_urls());
+    }
+}
--- src/RedditCrawler.java
+++ src/RedditCrawler.java
@@ -1,45 +1,83 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
-
-public class RedditCrawler {
+/*
+ * grab all links and store into hashtable
+ * send a list of different tags 
+ * store each tag in the hashtable
+ * each 
+ * 
+ */
+public class RedditCrawler implements Runnable {
     private String url;
+    private String tagType;
     private Document doc;
+    Thread runner;
+    static List imgur = new ArrayList();
     
-    public RedditCrawler(String url) throws IOException {
+    public RedditCrawler(String url, String tagType) throws IOException {
         this.url = url;
+        this.tagType = tagType;
+        runner = new Thread(this, tagType);
         this.doc = connect();
+        runner.start();
+    }
+    
+    public void saveTagType(Elements items) {
+       for(Element i : items) {
+           imgur.add(i.attr("abs:href"));                     
+       }
+    }
+
+    public void run() {
+       Elements item = this.docSelect(tagType);
+       //this.saveTagType(item);
+       System.out.println(item);
     }
     
     public Document connect() throws IOException {
         return Jsoup.connect(url).get();
     }
     
-    public String getTitle() {
-        return this.doc.title();
+    /*
+     * query webpage
+     */
+    public Elements docSelect(String tag) {
+        return this.doc.select(tag);
     }
+
     
-    public Elements getTag(String tag) {
-        return this.doc.getElementsByTag(tag);
-    }
-    
-    public Element getBody() {
-        return this.doc.body();
-    }
-    
-    public static void main(String args[]) {        
-        String url = "http://google.com";
-        RedditCrawler rc;
-       
+    public static void main(String args[]) throws IOException{    
+        // take a tag and search webpage using thread
+        String url = "http://www.reddit.com/r/Dota2";
+        String url2 = "http://www.reddit.com/r/HeroesOfNewerth";
+        String url3 = "http://www.reddit.com/r/science";
+        Thread t1 = new Thread(new RedditCrawler(url, "a[href]"));
+        Thread t2 = new Thread(new RedditCrawler(url2, "a[href]"));
+        //Thread t3 = new Thread(new RedditCrawler(url3, "a[href]"));
+        t1.start();
+        t2.start();
+        //t3.start();
         try {
-            rc = new RedditCrawler(url);
-            System.out.println("Title is :: " + rc.getTag("title"));            
-        } catch (IOException e) {
-            e.printStackTrace();         
-        }                
+            Thread.currentThread(); //.sleep(1000);
+            Thread.sleep(1000);
+        } catch(InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread()); 
+        //for(int i = 0; i < imgur.size(); i++ ) {            
+           // String tmp = imgur.get(i).toString();
+            //System.out.println(tmp);
+            //if(tmp.substring(7, 13))
+//        }
+        
+        
     }
     
     private static void usage() {
